1a09d20bf391a1d3f0d202c8ad61ad09ccaa7a5f
tobi adegoroye <tobiadegoroye@tobis-mbp.home>
2019-12-20 20:08:02 +0000

    first commit

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,12 +1,18 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:odd_one_out/InputScreen.dart';
+
+var inputScreen = new InputScreen();
+
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         backgroundColor: Colors.grey[600],
+        drawer: Drawer(
+        ),
         appBar: AppBar(
           title: Text("Odd One Out", style: TextStyle(color: Colors.black),),
           backgroundColor: Colors.amber[100],
@@ -30,57 +36,41 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
 
-
-
-
-
                     new Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 20.0, right: 5.0, top: 10.0),
-
-                            child: new Container(
-                                margin: EdgeInsets.fromLTRB(0, 400, 0, 0),
-                                alignment: Alignment.center,
-                                height: 60.0,
-                                decoration: new BoxDecoration(
-                                    color: Colors.amberAccent,
-                                    borderRadius: new BorderRadius.circular(9.0)),
-                                child: new Text("Tutorial",
-                                    style: new TextStyle(
-                                        fontSize: 20.0, color: Colors.black))),
-                          ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 40.0, right: 0,top: 400.0),
+                        child: ButtonTheme(
+                            buttonColor: Colors.amberAccent[200],
+                            minWidth: 150.0,
+                        height: 50.0,
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context){
+                              return InputScreen();
+                            }));
+                          },
+                          child: Text("Start"),
+                        )),
+                      ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 60.0, right: 0,top: 400.0),
+                          child: ButtonTheme(
+                            buttonColor: Colors.amberAccent[200],
+                              minWidth: 150.0,
+                              height: 50.0,
+                              child: RaisedButton(
+                                onPressed: () {},
+                                child: Text("Tutorial"),
+                              )),
                         ),
-                        Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 10.0, right: 20.0, top: 10.0),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(context, MaterialPageRoute(
-//                                    builder: (context) => Page2(),
-                                  ));
-                                },
-                                child: new Container(
-                                    margin: EdgeInsets.fromLTRB(0, 400, 0, 0),
-                                    alignment: Alignment.center,
-                                    height: 60.0,
-                                    decoration: new BoxDecoration(
-                                        color: Colors.amberAccent,
-                                        borderRadius: new BorderRadius.circular(9.0)),
-                                    child: new Text("Start",
-                                        style: new TextStyle(
-                                            fontSize: 20.0, color: Colors.black))),
-                              ),
-                            )
-                        )],
+                       ],
                     )
                   ],
                 ),
               ]),
         ));
   }
-}
+
+ }
--- lib/InputScreen.dart
+++ lib/InputScreen.dart
@@ -5,11 +5,8 @@ import 'package:flutter/material.dart';
 
 class InputScreen extends StatelessWidget {
   @override
-  Color hexToColor(String code) {
-    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-  }
 
-  String name;
+
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[600],
@@ -21,6 +18,149 @@ class InputScreen extends StatelessWidget {
           centerTitle: true,
           iconTheme: new IconThemeData(color: Colors.black),
         ),
+
+      body: Center(
+
+       child: new ListView(
+           padding: const EdgeInsets.only(top: 50.0,right: 50.0, left: 50.0, bottom: 50.0),
+            children: <Widget>[
+
+              new TextField(
+                decoration: new InputDecoration(
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(20.0),
+                      ),
+                    ),
+                    filled: true,
+                    hintStyle: new TextStyle(color: Colors.grey[800]),
+                    hintText: "Your Name",
+                    fillColor: Colors.amber[100]),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(20.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.grey[800]),
+                      hintText: "Your Name",
+                      fillColor: Colors.amber[100]),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(20.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.grey[800]),
+                      hintText: "Your Name",
+                      fillColor: Colors.amber[100]),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(20.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.grey[800]),
+                      hintText: "Your Name",
+                      fillColor: Colors.amber[100]),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(20.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.grey[800]),
+                      hintText: "Your Name",
+                      fillColor: Colors.amber[100]),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(20.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.grey[800]),
+                      hintText: "Your Name",
+                      fillColor: Colors.amber[100]),
+                ),
+              ),
+
+
+
+
+            ],
+
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+
+           generateForm();
+        },
+        label: Text('Add new input form'),
+        icon: Icon(Icons.add),
+        backgroundColor:  Colors.black,
+      ),
+
     );
   }
+}
+
+void generateForm(){
+  Container(
+    margin: EdgeInsets.only(top: 30.0),
+    child: new TextField(
+
+      decoration: new InputDecoration(
+          border: new OutlineInputBorder(
+            borderRadius: const BorderRadius.all(
+              const Radius.circular(20.0),
+            ),
+          ),
+          decoration: InputDecoration(
+            suffix: IconButton(
+              icon: Icon(Icons.cancel),
+             ),
+          ),
+          filled: true,
+          hintStyle: new TextStyle(color: Colors.grey[800]),
+          hintText: "Your Name",
+          fillColor: Colors.amber[100]),
+    ),
+
+  );
+
 }
